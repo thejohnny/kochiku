71d178c2f764d7430a9789c5eb1d1d8593c70267
John M. Carlin <thejohnny@gmail.com>
2015-03-17 10:25:44 -0500

    Fake production settings for testing in Vagrant

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,8 @@
+set :ssh_options, { keys: ['/Users/john/Projects/FrontDesk/kochiku/.vagrant/machines/default/parallels/private_key'] }
+
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Server that is running the Kochiku Rails app
-server 'kochiku.example.com', user: 'kochiku', roles: %w{web app db worker}
+#server 'kochiku.example.com', user: 'kochiku', roles: %w{web app db worker}
+server '10.211.55.66', user: 'vagrant', roles: %w{web app db worker}
